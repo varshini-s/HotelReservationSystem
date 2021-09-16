a216b6a98904b2b938a6a594eed273742ed4a265
varshini <dreamvarshini@gmail.com>
2021-09-17 00:45:24 +0530

    ADDED: test case to check date has proper format

--- src/test/java/com/bridgelabz/hotelreservation/HotelReservationTest.java
+++ src/test/java/com/bridgelabz/hotelreservation/HotelReservationTest.java
@@ -3,7 +3,9 @@ package com.bridgelabz.hotelreservation;
 import static org.junit.Assert.*;
 
 import java.time.LocalDate;
+import java.time.Month;
 import java.time.Period;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.List;
 import org.junit.Assert;
@@ -53,20 +55,32 @@ public class HotelReservationTest
 		hotelList.addHotel("BridgeWood", 150,50,110,50,4);
 		hotelList.addHotel("RidgeWood", 220,150,100,40,5);
 
-		LocalDate initialDate = LocalDate.parse("2021-09-11");
-		LocalDate finalDate = LocalDate.parse("2021-09-12");
+		String initialDate="11Sep2021";
+		String finalDate="12Sep2021";
+		
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("ddMMMyyyy");
+		LocalDate starteDate = LocalDate.parse(initialDate, formatter);
+		LocalDate endDate = LocalDate.parse(finalDate, formatter);
+		
 
-		List<Hotel> cheapHotels=hotelList.findCheapestHotelsList(initialDate,finalDate,"regular");
+		Calender calenderOperations = new Calender();
+		if(calenderOperations.validateFormatOfDate(finalDate)&&calenderOperations.validateFormatOfDate(initialDate))
+		{
+			
+			List<Hotel> cheapHotels=hotelList.findCheapestHotelsList(starteDate,endDate,"regular");
 
-		//expected list of cheap hotels
-		HotelReservationSystemImpl cheapHotelExpected = new HotelReservationSystemImpl();
-		cheapHotelExpected.addHotel("LakeWood", 110,90,80,80,3);
-		cheapHotelExpected.addHotel("BridgeWood", 150,50,110,50,4);
+			//expected list of cheap hotels
+			HotelReservationSystemImpl cheapHotelExpected = new HotelReservationSystemImpl();
+			cheapHotelExpected.addHotel("LakeWood", 110,90,80,80,3);
+			cheapHotelExpected.addHotel("BridgeWood", 150,50,110,50,4);
 
-		Assert.assertEquals(cheapHotels, cheapHotelExpected.hotelList);
-		Assert.assertEquals(hotelList.findCheapestHotelsList(initialDate,finalDate,"regular").get(0).getRegularCustomerCost(initialDate,finalDate),200);
+			Assert.assertEquals(cheapHotels, cheapHotelExpected.hotelList);
+			Assert.assertEquals(hotelList.findCheapestHotelsList(starteDate,endDate,"regular").get(0).getRegularCustomerCost(starteDate,endDate),200);
 
-		hotelList.findCheapHotelWithBestRating(initialDate, finalDate,"regular");
+			hotelList.findCheapHotelWithBestRating(starteDate,endDate,"regular");
+			
+		}
+		
 	}
 
 	@Test
@@ -135,19 +149,30 @@ public class HotelReservationTest
 	@Test
 	public void whenGivenDateRangeForRewardCustomer_ShouldReturnCheapestHotelWithBestRating()
 	{
+		
 		HotelReservationSystemImpl hotelList = new HotelReservationSystemImpl();
 		hotelList.addHotel("LakeWood", 110,90,80,80,3);
 		hotelList.addHotel("BridgeWood", 150,50,110,50,4);
 		hotelList.addHotel("RidgeWood", 220,150,100,40,5);
 
-		LocalDate initialDate = LocalDate.parse("2021-09-11");
-		LocalDate finalDate = LocalDate.parse("2021-09-12");
+		String initialDate="11Sep2021";
+		String finalDate="12Sep2021";
+		
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("ddMMMyyyy");
+		LocalDate starteDate = LocalDate.parse(initialDate, formatter);
+		LocalDate endDate = LocalDate.parse(finalDate, formatter);
+		
 
-		Hotel cheapestHotel=hotelList.findCheapHotelWithBestRating(initialDate, finalDate,"reward");
+		Calender calenderOperations = new Calender();
+		if(calenderOperations.validateFormatOfDate(finalDate)&&calenderOperations.validateFormatOfDate(initialDate))
+		{
+
+		Hotel cheapestHotel=hotelList.findCheapHotelWithBestRating(starteDate, endDate,"reward");
 
 		Assert.assertEquals("RidgeWood",cheapestHotel.getName());
 		Assert.assertEquals(5,cheapestHotel.getRating());
-		Assert.assertEquals(140,cheapestHotel.getRewardCustomerCost(initialDate, finalDate));
+		Assert.assertEquals(140,cheapestHotel.getRewardCustomerCost(starteDate, endDate));
+		}
 	}
 	@Test
 	public void whenEnteredNullCustomerType_ShouldThrowException()
@@ -192,6 +217,26 @@ public class HotelReservationTest
 		{
 			Assert.assertEquals(ExceptionType.ENTERED_EMPTY ,e.type);
 		}
+		
+	}
+	
+	@Test
+	public void whenGivenInvalidDateFormat_ShouldReturnFalse()
+	{
+		String date="10-Augu-2020";
+		Calender calenderOperation = new Calender();
+		boolean isValid=calenderOperation.validateFormatOfDate(date);
+		Assert.assertFalse(isValid);
+		
+	}
+	
+	@Test
+	public void whenGivenvalidDateFormat_ShouldReturnTrue()
+	{
+		String date="10Jan2020";
+		Calender calenderOperation = new Calender();
+		boolean isValid=calenderOperation.validateFormatOfDate(date);
+		Assert.assertTrue(isValid);
 		
 	}
 	
