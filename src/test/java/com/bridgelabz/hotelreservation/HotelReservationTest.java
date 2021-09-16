4b2694fc73ce392d7eff4bda83fd75dd9ab92326
varshini <dreamvarshini@gmail.com>
2021-09-16 22:27:46 +0530

    ADDED: test case to check reward customer rates added

--- src/test/java/com/bridgelabz/hotelreservation/HotelReservationTest.java
+++ src/test/java/com/bridgelabz/hotelreservation/HotelReservationTest.java
@@ -16,25 +16,37 @@ public class HotelReservationTest
 	public void whenAddedNewHotel_ItMustBeAddedToList() 
 	{
 		HotelReservationSystemImpl hotelList = new HotelReservationSystemImpl();
-		hotelList.addHotel("LakeWood", 110,90,3);
-		hotelList.addHotel("BridgeWood", 150,50,4);
-		hotelList.addHotel("RidgeWood", 220,150,5);
+		hotelList.addHotel("LakeWood", 110,90,80,80,3);
+		hotelList.addHotel("BridgeWood", 150,50,110,50,4);
+		hotelList.addHotel("RidgeWood", 220,150,100,40,5);
 
 		Hotel hotel=hotelList.getHotel("LakeWood");
-		assertEquals(hotelList.getHotel("LakeWood"), new Hotel("LakeWood", 110,90,3));
-		assertEquals(hotelList.getHotel("BridgeWood"), new Hotel("BridgeWood", 150,50,4));
-		assertEquals(hotelList.getHotel("RidgeWood"), new Hotel("RidgeWood", 220,150,5));
+		assertEquals(hotelList.getHotel("LakeWood"), new Hotel("LakeWood", 110,90,80,80,3));
+		assertEquals(hotelList.getHotel("BridgeWood"),new Hotel ("BridgeWood", 150,50,110,50,4));
+		assertEquals(hotelList.getHotel("RidgeWood"), new Hotel("RidgeWood", 220,150,100,40,5));
 
 
 	}
 
 	@Test
+	public void whenGivenRegularCustomerRates_ShouldBeAddedToGivenHotel()
+	{
+		HotelReservationSystemImpl hotelList = new HotelReservationSystemImpl();
+		hotelList.addHotel("LakeWood", 110,90,80,80,3);
+		hotelList.addHotel("BridgeWood", 150,50,110,50,4);
+		hotelList.addHotel("RidgeWood", 220,150,100,40,5);
+
+		Hotel hotel=hotelList.getHotel("LakeWood");
+		assertEquals(hotel.getRegularCustomerWeekDayRate(), 110);
+		assertEquals(hotel.getRegularCustomerWeekEndRate(), 90);
+	}
+	@Test
 	public void whenGivenDateRange_ShouldReturnListOfCheapestHotel()
 	{
 		HotelReservationSystemImpl hotelList = new HotelReservationSystemImpl();
-		hotelList.addHotel("LakeWood", 110,90,3);
-		hotelList.addHotel("BridgeWood", 150,50,4);
-		hotelList.addHotel("RidgeWood", 220,150,5);
+		hotelList.addHotel("LakeWood", 110,90,80,80,3);
+		hotelList.addHotel("BridgeWood", 150,50,110,50,4);
+		hotelList.addHotel("RidgeWood", 220,150,100,40,5);
 
 		LocalDate initialDate = LocalDate.parse("2021-09-11");
 		LocalDate finalDate = LocalDate.parse("2021-09-12");
@@ -43,33 +55,33 @@ public class HotelReservationTest
 
 		//expected list of cheap hotels
 		HotelReservationSystemImpl cheapHotelExpected = new HotelReservationSystemImpl();
-		cheapHotelExpected.addHotel("LakeWood", 110,90,3);
-		cheapHotelExpected.addHotel("BridgeWood", 150,50,4);
+		cheapHotelExpected.addHotel("LakeWood", 110,90,80,80,3);
+		cheapHotelExpected.addHotel("BridgeWood", 150,50,110,50,4);
 
 		assertEquals(cheapHotels, cheapHotelExpected.hotelList);
 		assertEquals(hotelList.findCheapestHotelsList(initialDate,finalDate).get(0).getRegularCustomerCost(initialDate,finalDate),200);
-		
+
 		hotelList.findCheapHotelWithBestRating(initialDate, finalDate);
 	}
-	
+
 	@Test
-	public void whenAddedRating_ShouldbeAddedToRespectiveHotels()
+	public void whenGivenRating_ShouldbeAddedToRespectiveHotels()
 	{
 		HotelReservationSystemImpl hotelList = new HotelReservationSystemImpl();
-		hotelList.addHotel("LakeWood", 110,90,3);
-		
+		hotelList.addHotel("LakeWood", 110,90,80,80,3);
+
 		Hotel hotel=hotelList.getHotel("LakeWood");
 		assertEquals(3, hotel.getRating());
-		
+
 	}
-	
+
 	@Test
 	public void whenGivenDateRange_ShouldReturnCheapestHotelWithBestRating()
 	{
 		HotelReservationSystemImpl hotelList = new HotelReservationSystemImpl();
-		hotelList.addHotel("LakeWood", 110,90,3);
-		hotelList.addHotel("BridgeWood", 150,50,4);
-		hotelList.addHotel("RidgeWood", 220,150,5);
+		hotelList.addHotel("LakeWood", 110,90,80,80,3);
+		hotelList.addHotel("BridgeWood", 150,50,110,50,4);
+		hotelList.addHotel("RidgeWood", 220,150,100,40,5);
 
 		LocalDate initialDate = LocalDate.parse("2021-09-11");
 		LocalDate finalDate = LocalDate.parse("2021-09-12");
@@ -80,14 +92,14 @@ public class HotelReservationTest
 		assertEquals(4,cheapestHotel.getRating());
 		assertEquals(200,cheapestHotel.getRegularCustomerCost(initialDate, finalDate));
 	}
-	
+
 	@Test
 	public void whenGivenDateRange_ShouldReturnBestRatedHotel()
 	{
 		HotelReservationSystemImpl hotelList = new HotelReservationSystemImpl();
-		hotelList.addHotel("LakeWood", 110,90,3);
-		hotelList.addHotel("BridgeWood", 150,50,4);
-		hotelList.addHotel("RidgeWood", 220,150,5);
+		hotelList.addHotel("LakeWood", 110,90,80,80,3);
+		hotelList.addHotel("BridgeWood", 150,50,110,50,4);
+		hotelList.addHotel("RidgeWood", 220,150,100,40,5);
 
 		LocalDate initialDate = LocalDate.parse("2021-09-11");
 		LocalDate finalDate = LocalDate.parse("2021-09-12");
@@ -97,8 +109,21 @@ public class HotelReservationTest
 		assertEquals("RidgeWood",cheapestHotel.getName());
 		assertEquals(5,cheapestHotel.getRating());
 		assertEquals(370,cheapestHotel.getRegularCustomerCost(initialDate, finalDate));
-		
-		
+
+
+	}
+	
+	@Test
+	public void whenGivenRewardCustomerRates_ShouldBeAddedToGivenHotel()
+	{
+		HotelReservationSystemImpl hotelList = new HotelReservationSystemImpl();
+		hotelList.addHotel("LakeWood", 110,90,80,80,3);
+		hotelList.addHotel("BridgeWood", 150,50,110,50,4);
+		hotelList.addHotel("RidgeWood", 220,150,100,40,5);
+
+		Hotel hotel=hotelList.getHotel("LakeWood");
+		assertEquals(hotel.getRewardCustomerWeekDayRate(),80);
+		assertEquals(hotel.getRewardCustomerWeekEndrate(),80);
 	}
 
 
