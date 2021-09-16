338a5678df2c1708ecc7634536b5bf618f32b509
varshini <dreamvarshini@gmail.com>
2021-09-16 13:32:58 +0530

    ADDED:implementing the methods defined in interface

---
+++ src/main/java/com/bridgelabz/hotelreservation/HotelReservationSystemImpl.java
@@ -0,0 +1,48 @@
+package com.bridgelabz.hotelreservation;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Objects;
+
+
+public class HotelReservationSystemImpl implements HotelReservationSystemIF
+{
+	List<Hotel> hotelList; 
+
+	public HotelReservationSystemImpl() 
+	{
+		hotelList= new ArrayList<Hotel>();
+	}
+
+	@Override
+	public void addHotel(String name, int regularCustomerRate) 
+	{
+		hotelList.add(new Hotel(name,regularCustomerRate));
+
+	}
+
+	@Override
+	public Hotel getHotel(String name) 
+	{
+		
+		return hotelList.stream()
+				.filter(hotel -> Objects.equals(hotel.getName(),name))
+				.findFirst()
+				.orElse(null);	
+	}
+
+	@Override
+	public Hotel findCheapestHotel(int numberOfDays) 
+
+	{
+		Hotel cheapHotel=hotelList.stream().min((firstHotel, secondHotel) -> Integer.compare(firstHotel.getCost(numberOfDays), secondHotel.getCost(numberOfDays))).get();
+		return cheapHotel;
+	}
+
+
+
+
+
+}
