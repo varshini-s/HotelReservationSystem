aecd9215960a2eafab280ce9bad907d320a3e6b6
varshini <dreamvarshini@gmail.com>
2021-09-16 09:02:24 +0530

    ADDED: hotel and customer classes defined

---
+++ src/main/java/com/bridgelabz/hotelreservation/Customer.java
@@ -0,0 +1,45 @@
+package com.bridgelabz.hotelreservation;
+
+public class Customer 
+{
+
+	private String name;
+	private String age;
+	private String phoneNumber;
+	private String typeOfCustomer;
+	
+	public String getName() 
+	{
+		return name;
+	}
+	public void setName(String name) 
+	{
+		this.name = name;
+	}
+	public String getAge() 
+	{
+		return age;
+	}
+	public void setAge(String age) 
+	{
+		this.age = age;
+	}
+	public String getPhoneNumber() 
+	{
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) 
+	{
+		this.phoneNumber = phoneNumber;
+	}
+	public String getTypeOfCustomer() 
+	{
+		return typeOfCustomer;
+	}
+	public void setTypeOfCustomer(String typeOfCustomer)
+	{
+		this.typeOfCustomer = typeOfCustomer;
+	}
+	
+	
+}
