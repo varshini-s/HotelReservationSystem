8bfbbe4d299878ae80746c219be64cf6a56e7484
varshini <dreamvarshini@gmail.com>
2021-09-16 21:22:11 +0530

    ADDED: test case to check rating added

--- src/test/java/com/bridgelabz/hotelreservation/HotelReservationTest.java
+++ src/test/java/com/bridgelabz/hotelreservation/HotelReservationTest.java
@@ -16,14 +16,14 @@ public class HotelReservationTest
 	public void whenAddedNewHotel_ItMustBeAddedToList() 
 	{
 		HotelReservationSystemImpl hotelList = new HotelReservationSystemImpl();
-		hotelList.addHotel("LakeWood", 110,90);
-		hotelList.addHotel("BridgeWood", 150,50);
-		hotelList.addHotel("RidgeWood", 220,150);
+		hotelList.addHotel("LakeWood", 110,90,3);
+		hotelList.addHotel("BridgeWood", 150,50,4);
+		hotelList.addHotel("RidgeWood", 220,150,5);
 
 		Hotel hotel=hotelList.getHotel("LakeWood");
-		assertEquals(hotelList.getHotel("LakeWood"), new Hotel("LakeWood", 110,90));
-		assertEquals(hotelList.getHotel("BridgeWood"), new Hotel("BridgeWood", 150,50));
-		assertEquals(hotelList.getHotel("RidgeWood"), new Hotel("RidgeWood", 220,150));
+		assertEquals(hotelList.getHotel("LakeWood"), new Hotel("LakeWood", 110,90,3));
+		assertEquals(hotelList.getHotel("BridgeWood"), new Hotel("BridgeWood", 150,50,4));
+		assertEquals(hotelList.getHotel("RidgeWood"), new Hotel("RidgeWood", 220,150,5));
 
 
 	}
@@ -32,9 +32,9 @@ public class HotelReservationTest
 	public void whenGivenHotelsList_ShouldReturnCheapestHotel()
 	{
 		HotelReservationSystemImpl hotelList = new HotelReservationSystemImpl();
-		hotelList.addHotel("LakeWood", 110,90);
-		hotelList.addHotel("BridgeWood", 150,50);
-		hotelList.addHotel("RidgeWood", 220,150);
+		hotelList.addHotel("LakeWood", 110,90,3);
+		hotelList.addHotel("BridgeWood", 150,50,4);
+		hotelList.addHotel("RidgeWood", 220,150,5);
 
 		LocalDate initialDate = LocalDate.parse("2021-09-11");
 		LocalDate finalDate = LocalDate.parse("2021-09-12");
@@ -43,11 +43,22 @@ public class HotelReservationTest
 
 		//expected list of cheap hotels
 		HotelReservationSystemImpl cheapHotelExpected = new HotelReservationSystemImpl();
-		cheapHotelExpected.addHotel("LakeWood", 110,90);
-		cheapHotelExpected.addHotel("BridgeWood", 150,50);
+		cheapHotelExpected.addHotel("LakeWood", 110,90,3);
+		cheapHotelExpected.addHotel("BridgeWood", 150,50,4);
 
 		assertEquals(cheapHotels, cheapHotelExpected.hotelList);
 		assertEquals(hotelList.findCheapestHotel(initialDate,finalDate).get(0).getRegularCustomerCost(initialDate,finalDate),200);
+	}
+	
+	@Test
+	public void whenAddedRating_ShouldbeAddedToRespectiveHotels()
+	{
+		HotelReservationSystemImpl hotelList = new HotelReservationSystemImpl();
+		hotelList.addHotel("LakeWood", 110,90,3);
+		
+		Hotel hotel=hotelList.getHotel("LakeWood");
+		assertEquals(3, hotel.getRating());
+		
 	}
 
 
