f2ba05fa8162fd9c684c73316641df62ceb8975e
varshini <dreamvarshini@gmail.com>
2021-09-17 17:51:35 +0530

    REFACTORED: changing customer rate variable to hashmap

--- src/main/java/com/bridgelabz/hotelreservation/Hotel.java
+++ src/main/java/com/bridgelabz/hotelreservation/Hotel.java
@@ -1,47 +1,32 @@
 package com.bridgelabz.hotelreservation;
 
-import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class Hotel 
 {
 	private String name;
-	private int regularCustomerWeekDayRate;
-	private int regularCustomerWeekEndRate;
-	private int rewardCustomerWeekDayRate;
-	private int rewardCustomerWeekEndRate;
 	private int rating;
+	private Map<Customer.CustomerType,Map<DateServiceProvider.DayType,Long>> CustomerRates;
+	Map<DateServiceProvider.DayType,Long> regularCustomerRates =new HashMap<DateServiceProvider.DayType, Long>();
+	Map<DateServiceProvider.DayType,Long> rewardCustomerRates=new HashMap<DateServiceProvider.DayType, Long>();
+	DateServiceProvider calender = new DateServiceProvider();
 
 
-	public Hotel(String name,int regularCustomerWeekDayRate,int regularCustomerWeekEndRate,int rewardCustomerWeekDayRate,int rewardCustomerWeekEndRate, int rating ) 
+	public Hotel(String name,long regularCustomerWeekDayRate,long regularCustomerWeekEndRate,long rewardCustomerWeekDayRate,long rewardCustomerWeekEndRate, int rating ) 
 	{
 		this.name=name;
-		this.regularCustomerWeekDayRate=regularCustomerWeekDayRate;
-		this.regularCustomerWeekEndRate=regularCustomerWeekEndRate;
-		this.rewardCustomerWeekDayRate=rewardCustomerWeekDayRate;
-		this.rewardCustomerWeekEndRate=rewardCustomerWeekEndRate;
+		this.CustomerRates=new HashMap<Customer.CustomerType, Map<DateServiceProvider.DayType,Long>>();
+
+		regularCustomerRates.put(DateServiceProvider.DayType.WEEKDAY, regularCustomerWeekDayRate);
+		regularCustomerRates.put(DateServiceProvider.DayType.WEEKEND, regularCustomerWeekEndRate);
+		this.CustomerRates.put(Customer.CustomerType.REGULAR_CUSTOMER,regularCustomerRates);
+		rewardCustomerRates.put(DateServiceProvider.DayType.WEEKDAY, rewardCustomerWeekDayRate);
+		rewardCustomerRates.put(DateServiceProvider.DayType.WEEKEND, rewardCustomerWeekEndRate);
+		this.CustomerRates.put(Customer.CustomerType.REWARD_CUSTOMER,rewardCustomerRates);
+
 		this.rating=rating;
-
-	}
-
-	public int getRewardCustomerWeekDayRate() 
-	{
-		return rewardCustomerWeekDayRate;
-	}
-
-	public void setRewardCustomerWeekDayRate(int rewardCustomerWeekDay) 
-	{
-		this.rewardCustomerWeekDayRate = rewardCustomerWeekDay;
-	}
-
-	public int getRewardCustomerWeekEndrate() 
-	{
-		return rewardCustomerWeekEndRate;
-	}
-
-	public void setRewardCustomerWeekEndrate(int rewardCustomerWeekEndrate)
-	{
-		this.rewardCustomerWeekEndRate = rewardCustomerWeekEndrate;
 	}
 
 	public String getName() 
@@ -53,58 +38,80 @@ public class Hotel
 	{
 		this.name = name;
 	}
-
-	public int getRegularCustomerWeekDayRate() 
+	public int getRating() 
 	{
-		return regularCustomerWeekDayRate;
-	}
-
-	public void setRegularCustomerWeekDayRate(int regularCustomerWeekDayRate) {
-		this.regularCustomerWeekDayRate = regularCustomerWeekDayRate;
-	}
-
-	public int getRegularCustomerWeekEndRate() 
-	{
-		return regularCustomerWeekEndRate;
-	}
-
-	public void setRegularCustomerWeekEndRate(int regularCustomerWeekEndRate) 
-	{
-		this.regularCustomerWeekEndRate = regularCustomerWeekEndRate;
-	}
-
-
-	public int getRating() {
 		return rating;
 	}
 
-	public void setRating(int rating) {
+	public void setRating(int rating) 
+	{
 		this.rating = rating;
 	}
 
-
-	public int getRegularCustomerCost(LocalDate initialDate,LocalDate finalDate)
+	public long getRewardCustomerWeekDayRate() 
 	{
-
-		Calender calender = new Calender();
-		calender.setInitialDate(initialDate);
-		calender.setFinalDate(finalDate);
-		int numberOfWeekDays=calender.getNumberOfWeekDays();
-		int numberOfWeekends=calender.getNumberOfWeekends();
-
-		return (this.regularCustomerWeekDayRate*numberOfWeekDays)+(this.regularCustomerWeekEndRate*numberOfWeekends);
+		return CustomerRates.get(Customer.CustomerType.REWARD_CUSTOMER).get(DateServiceProvider.DayType.WEEKDAY);
 	}
-	
-	public int getRewardCustomerCost(LocalDate initialDate,LocalDate finalDate)
-	{
 
-		Calender calender = new Calender();
+	public void setRewardCustomerWeekDayRate(long rewardCustomerWeekDay) 
+	{
+		rewardCustomerRates.put(DateServiceProvider.DayType.WEEKDAY, rewardCustomerWeekDay);
+		this.CustomerRates.put(Customer.CustomerType.REWARD_CUSTOMER,rewardCustomerRates);
+	}
+
+	public long getRewardCustomerWeekEndrate() 
+	{
+		return CustomerRates.get(Customer.CustomerType.REWARD_CUSTOMER).get(DateServiceProvider.DayType.WEEKEND);
+	}
+
+	public void setRewardCustomerWeekEndrate(long rewardCustomerWeekEndrate)
+	{
+		rewardCustomerRates.put(DateServiceProvider.DayType.WEEKEND, rewardCustomerWeekEndrate);
+		this.CustomerRates.put(Customer.CustomerType.REWARD_CUSTOMER,rewardCustomerRates);
+	}
+
+	public long getRegularCustomerWeekDayRate() 
+	{
+		return CustomerRates.get(Customer.CustomerType.REGULAR_CUSTOMER).get(DateServiceProvider.DayType.WEEKDAY);
+	}
+
+	public void setRegularCustomerWeekDayRate(long regularCustomerWeekDayRate) 
+	{
+		rewardCustomerRates.put(DateServiceProvider.DayType.WEEKDAY, regularCustomerWeekDayRate);
+		this.CustomerRates.put(Customer.CustomerType.REGULAR_CUSTOMER,rewardCustomerRates);
+	}
+
+	public long getRegularCustomerWeekEndRate() 
+	{
+		return CustomerRates.get(Customer.CustomerType.REGULAR_CUSTOMER).get(DateServiceProvider.DayType.WEEKEND);
+	}
+
+	public void setRegularCustomerWeekEndRate(long regularCustomerWeekEndRate) 
+	{
+		rewardCustomerRates.put(DateServiceProvider.DayType.WEEKEND, regularCustomerWeekEndRate);
+		this.CustomerRates.put(Customer.CustomerType.REGULAR_CUSTOMER,rewardCustomerRates);
+	}
+
+	public long getRegularCustomerCost(String initialDate,String finalDate)
+	{
 		calender.setInitialDate(initialDate);
 		calender.setFinalDate(finalDate);
 		int numberOfWeekDays=calender.getNumberOfWeekDays();
 		int numberOfWeekends=calender.getNumberOfWeekends();
 
-		return (this.rewardCustomerWeekDayRate*numberOfWeekDays)+(this.rewardCustomerWeekEndRate*numberOfWeekends);
+		return (Long)(this.getRegularCustomerWeekDayRate()*numberOfWeekDays)+(this.getRegularCustomerWeekEndRate()*numberOfWeekends);
+	}
+
+	public long getRewardCustomerCost(String initialDate,String finalDate)
+	{
+
+		calender.setInitialDate(initialDate);
+		calender.setFinalDate(finalDate);
+
+		int numberOfWeekDays=calender.getNumberOfWeekDays();
+		int numberOfWeekends=calender.getNumberOfWeekends();
+
+		return (this.getRewardCustomerWeekDayRate()*numberOfWeekDays)+(this.getRewardCustomerWeekEndrate()*numberOfWeekends);
 	}
 
 
@@ -124,11 +131,8 @@ public class Hotel
 
 		final Hotel other = (Hotel) obj;
 
-	
-		if(this.name.equals(other.name)==false||(this.regularCustomerWeekDayRate!=other.regularCustomerWeekDayRate)
-				||this.regularCustomerWeekEndRate!=other.regularCustomerWeekEndRate
-				||this.rewardCustomerWeekDayRate!=other.rewardCustomerWeekDayRate
-				||this.rewardCustomerWeekEndRate!=other.rewardCustomerWeekEndRate
+
+		if(this.name.equals(other.name)==false||(this.CustomerRates.equals(other.CustomerRates)==false)
 				||this.rating!=other.rating)
 
 		{
