27260a0a5c10709ef2f0c5d7bca95a0fac65c2f9
varshini <dreamvarshini@gmail.com>
2021-09-17 17:56:55 +0530

    ADDED: main class to include welcome message

---
+++ src/main/java/com/bridgelabz/hotelreservation/HotelReservationMain.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.hotelreservation;
+
+public class HotelReservationMain 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome to hotel reservation system");
+
+	}
+
+}
